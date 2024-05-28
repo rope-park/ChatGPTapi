5fba58b6e142aef6a60156ed3adf2d373bac8f6c
rope-park <kelly6789@naver.com>
2024-05-28 16:25:08 +0900

    good answer

--- testAPI.py
+++ testAPI.py
@@ -123,7 +123,7 @@ tools = [
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
 
 # chatGPT API로 response 얻어오는 함수
-def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
+def chat_completion_request(messages, tools=tools, tool_choice=None, model=GPT_MODEL):
     # 함수의 specification 설정
     try:
         response = client.chat.completions.create(
